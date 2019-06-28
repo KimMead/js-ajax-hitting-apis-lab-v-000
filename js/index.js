977739e2beae5bed8562abc1a5c3bd2bf5c7a326
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-28 20:31:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,3 +12,18 @@ function getRepositories() {
   req.send()
   return false;
 }
+function displayRepositories(event, data) {
+  let repos = JSON.parse(this.responseText)
+
+  const repoList = "<ul>" + repos.map(repo => {
+    return(`
+      <li>
+        <h2><a href="${repo.html_url}">${repo.name}</a></h2>
+        <span><a href="#" data-repository="${repo.name}" data-username="${repo["owner"]["login"]}" onclick="getCommits(this)">Get Commits</a></span>
+        <span><a href="#" data-repository="${repo.name}" data-username="${repo["owner"]["login"]}" onclick="getBranches(this)">Get Branches</a></span>
+      </li>`
+    )
+  }).join('') + "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
+
