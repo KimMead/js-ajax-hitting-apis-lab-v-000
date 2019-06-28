a0dd2a3f4fb2124a9591119d44eab3b76760cc1c
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-28 20:24:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // your code here
+const gitURL = "https://api.github.com"
+
+function getRepositories() {
+  let userName = document.getElementById('username').value
+  let repoURL = `${gitURL}/users/${userName}/repos`
+
+  const req = new XMLHttpRequest()
+
+  req.addEventListener("load", displayRepositories)
+  req.open("GET", repoURL )
+  req.send()
+  return false;
+}
